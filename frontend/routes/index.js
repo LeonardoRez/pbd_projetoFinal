8b986733d4f2a702049a5d6eb9b83af909dbbe7c
Trabalhosuft <leonardorec1@gmail.com>
2017-11-27 15:17:26 -0300

    implementa a paginação na busca

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -2,43 +2,63 @@ var express = require('express');
 var router = express.Router();
 // var inicio = 1;
 // var fim = 20;
-var skip = 20;
+var skip = 50;
+var teste = "a";
+var total = 0;
 
 
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Home' });
-});
+// router.get('/', function (req, res, next) {
+//   res.render('index', { title: 'Home' });
+// });
 router.get('/helloworld', function (req, resp) {
   resp.render('helloworld', { title: 'Yo' });
 });
-router.get('/tabela', function (req, res) {
+router.get('/tabela/([0-9]*)?', function (req, res) {
+  var s = require('url').parse(req.url, true)['path'].split('/')[2];
+  if(s == "")
+    s=1;
+  // console.log(require('url').parse(req.url, true)['path'].split('/')[2]);  
   var db = req.db;
   var collection = db.get('pessoa');
-  var total = -1;
+  var docs;
+  total = -1;
   collection.count({}, function (error, count) {
     console.log(error, count);
     total = count;
   });
-  var documentos;
-  collection.find({}, { limit: skip, skip: skip*1 }, function (e, docs) {
-    console.log("\n\n\nteste\n\n\n");
-
-    console.log(docs);
-    console.log("\n\n\n\n");
-    for (var key in docs) {
-      let value = docs[key];
-      console.log(value);
-    }
-    console.log("\n\n\nteste\n\n\n");
-    
+  collection.find({}, { limit: skip, skip: skip * s-1 }, function (e, d) {
     res.render('lista', {
-      "titulo": "Tabela",
-      "documentos": docs,
-      "inicio": (1+skip*(0)),
-      "fim": (skip*1),
-      "total": total
+      "titulo": "Registros",
+      "documentos": d,
+      "inicio": (1 + skip * (s - 1)),
+      "fim": (skip * s),
+      "total": total,
+      "pg_atual": s,
+      "skip": skip
+    });
+  });
+});
+/* Home Page */
+router.get('/', function (req, res) {
+  var db = req.db;
+  var collection = db.get('pessoa');
+  var docs;
+  total = -1;
+  collection.count({}, function (error, count) {
+    console.log(error, count);
+    total = count;
+  });
+  collection.find({}, { limit: skip, skip: skip * 0 }, function (e, d) {
+    res.render('lista', {
+      "titulo": "Registros",
+      "documentos": d,
+      "inicio": (1 + skip * (0)),
+      "fim": (skip * 1),
+      "total": total,
+      "pg_atual": 1,
+      "skip": skip
     });
   });
 })
